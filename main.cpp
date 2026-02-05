64b720de2d7ce7c7c6e2f6e4b2b3c15bc1316dec
Sandi Chamberlain <schamb28@ut.utm.edu>
2026-02-05 11:55:49 -0600

    Updated main.cpp to ask for number then give sum function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n) {
+  int total = 0;
+  for (int i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}  
+
 int main() {
-  cout << "Hello World!" << endl;
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  int result = sum(n);
+  cout << "The sum from 1 to " << n << " is " << result << endl;
+  
   return 0;
 }
